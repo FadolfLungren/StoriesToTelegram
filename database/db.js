2673498381021cff38975fcc19adc50c53a81480
FadolfLungren <fadolfSatan671gmail.com>
2022-02-10 15:41:05 +0300

    fixed ви

--- database/db.js
+++ database/db.js
@@ -4,9 +4,15 @@ module.exports = new Sequelize(
     //process.env.DB_NAME,
     //process.env.DB_USER,
     //process.env.DB_PASSWORD,
-    process.env.DATABASE_URL+`?&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`,
+    process.env.DATABASE_URL,
     {
         dialect:"postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
+        }
         //host:process.env.DB_HOST,
         //port:process.env.DB_PORT
     }
